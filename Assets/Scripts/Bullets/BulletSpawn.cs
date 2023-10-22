439dc9a4344b7bcf93f3515f7b9f0e8a2a1243ba
Andre0y <andreysirotkin2009@gmail.com>
2023-10-22 10:29:49 +0300

    added logic for bullet spawn and bullet shoot

--- Assets/Scripts/Bullets/BulletSpawn.cs
+++ Assets/Scripts/Bullets/BulletSpawn.cs
@@ -1,34 +1,13 @@
+using System;
 using UnityEngine;
 
 public class BulletSpawn : MonoBehaviour
 {
     [SerializeField] private Bullet _bullet;
     [SerializeField] private Transform _bulletSpawnPoint;
-    [SerializeField] private float _speed;
-    private Bullet _bulletOnScene;
-    private Bullet _bulletForSpawn;
 
-    private void Update()
+    public Bullet BulletSpawning()
     {
-        BulletShot();
-        _bulletOnScene = BulletShot();
-
-        BulletDirection();
+        return Instantiate(_bullet, _bulletSpawnPoint.position, Quaternion.identity, transform); 
     }
-    
-    private Bullet BulletShot()
-    {
-        if (Input.GetKeyDown(KeyCode.Space)) 
-        { 
-            _bulletForSpawn = Instantiate(_bullet, _bulletSpawnPoint.position, Quaternion.identity, transform); 
-        }
-        return _bulletForSpawn;
-    }
-
-    private void BulletDirection()
-    {
-        _bulletOnScene.transform.Translate(Vector3.forward * Time.fixedDeltaTime * _speed);
-    }
-
-    
 }
